1daa0c41b4ca75a4af0b2836d51c2da44d5dd40c
Mohamed4k <aaamohammed603@gmail.com>
2025-02-02 21:01:22 +0200

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=ColoroXKernel by specific cat @SudoMohamed
+kernel.string=ColoroXKernel by @SudoMohamed
 do.devicecheck=1
 do.modules=0
 do.systemless=1
